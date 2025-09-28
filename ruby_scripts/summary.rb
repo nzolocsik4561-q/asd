705ca64bed93241e32580277ddcb58d8e236e82b
nzolocsik4561-q <sh088184.1@gmail.com>
2025-09-28 21:20:05 +0800

    Add Ruby word summary script

---
+++ ruby_scripts/summary.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+def summarize(text)
+  counts = Hash.new(0)
+  text.split.each { |word| counts[word.downcase] += 1 }
+  counts.sort_by { |word, count| [-count, word] }
+end
+
+if $PROGRAM_NAME == __FILE__
+  sample = "Codex codex demo script"
+  summarize(sample).each do |word, count|
+    puts format("%s => %d", word, count)
+  end
+end
